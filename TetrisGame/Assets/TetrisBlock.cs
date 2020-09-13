5405f52fac29fa3656ba379397e292597e4f96fd
pedroEifler <pedroqueeifler@gmail.com>
2020-09-13 18:10:54 -0300

    Moviment Tetramino

---
+++ TetrisGame/Assets/TetrisBlock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+    private float previousTime;
+    public float fallTime = 0.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow)){
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        
+        else if(Input.GetKeyDown(KeyCode.RightArrow)){
+            transform.position += new Vector3(1, 0, 0);
+        }
+
+        if(Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime)){
+            transform.position += new Vector3(0, -1, 0);
+            previousTime =  Time.time;
+        }
+    }
+}
