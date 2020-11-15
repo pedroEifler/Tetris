9abc5ab20927eb4ea1148d0880ab8afa938076d0
pedroEifler <pedroqueeifler@gmail.com>
2020-11-15 11:09:49 -0300

    Delete Line

--- TetrisGame/Assets/TetrisBlock.cs
+++ TetrisGame/Assets/TetrisBlock.cs
@@ -6,9 +6,9 @@ public class TetrisBlock : MonoBehaviour {
     public Vector3 rotationPoint;
     private float previousTime;
     public float fallTime = 0.8f;
-    private static int height = 20;
+    private static int height = 21;
     private static int widght = 10;
-    private static Transform[,] grid = new Transform[widght, height];
+    private static Transform[, ] grid = new Transform[widght, height];
 
     // Start is called before the first frame update 
     void Start () {
@@ -38,11 +38,49 @@ public class TetrisBlock : MonoBehaviour {
             transform.position += new Vector3 (0, -1, 0);
             if (!ValidMoviment ()) {
                 transform.position -= new Vector3 (0, -1, 0);
-                AddToGrid();
+                AddToGrid ();
+                CheckForLines();
                 this.enabled = false;
                 FindObjectOfType<SpawnTetramino> ().NewTetramino ();
             }
             previousTime = Time.time;
+        }
+    }
+
+    void CheckForLines () {
+        for (int i = 0; i < height - 1; i++) {
+            if (HasLine (i)) {
+                DeleteLine (i);
+                RowDown (i);
+            }
+        }
+    }
+
+    bool HasLine (int i) {
+        for (int j = 0; j < widght; j++) {
+            if (grid[j, i] == null)
+                return false;
+        }
+        return true;
+    }
+
+    void DeleteLine (int i) {
+        for (int j = 0; j < widght; j++) {
+            Destroy (grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+    }
+
+    void RowDown (int i) {
+        for (int y = i; y < height; y++) {
+            for (int j = 0; j < widght; j++) {
+                if (grid[j, y] != null) {
+                    grid[j, y - 1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y - 1].transform.position -= new Vector3 (0, 1, 0);
+                }
+            }
+
         }
     }
 
